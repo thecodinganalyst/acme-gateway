971f2d9879e092c29d0185ecd5d43ef4287cbf36
Dennis Cai <60729103+thecodinganalyst@users.noreply.github.com>
2025-04-21 21:50:49 +0800

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Generating kong.yml from environment variables..."
-
-envsubst < /usr/local/kong/declarative/kong.template.yml > /usr/local/kong/declarative/kong.yml
-
-echo "Starting Kong Gateway..."
-exec kong docker-start
